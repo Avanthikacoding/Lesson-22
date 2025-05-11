68b8f0d94f70a3e1edd115c8907fca834cd6cad2
Avanthikacoding <aviedayilliam@gmail.com>
2025-05-11 13:35:13 +0100

    Add files via upload

---
+++ Acivity 1.py
@@ -0,0 +1,17 @@
+tuplex = ("tuple" , False , 5,96)
+print(tuplex)
+tuple5 = (5,30,15,8,13,4)
+print(tuple5)
+
+
+tuple9 = tuple5 +(5,)
+print(tuple9)
+
+tuple7 = (5,7,15,17,15,9,10,15)
+print(tuple7.count(15))
+
+tuple3 = (2,8,6,10,4,7,45,3,67,55,5)
+slice = tuple3[3:8]
+print(slice)
+slice7 = tuple3[:-2]
+print(slice7)
